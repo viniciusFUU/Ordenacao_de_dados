5b22d3c98bfe96020f92479fd2f366fd86db1c49
Vinícius Monteiro <vinicius3_1998@hotmail.com>
2024-06-28 11:43:40 -0300

    insertionSort

---
+++ ordenacaoDeDados/ordenação/insertionSort.py
@@ -0,0 +1,17 @@
+lista = [3,1,5,9,8,2,4,7,6]
+
+def insertSort(vetor):
+    for i in range(1, len(vetor)):
+        posicaoParaInserir = i
+
+        valorAtual = vetor[i]
+        vetor.remove(valorAtual)
+
+        for j in range(i-1, -1, -1):
+            if vetor[j] > valorAtual:
+                posicaoParaInserir = j
+        vetor.insert(posicaoParaInserir, valorAtual)
+
+    return vetor
+
+print(insertSort(lista))
