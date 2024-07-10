bbb39e33fbe6ec2db59e7168c8047a452bbbdaa4
Vinícius Monteiro <vinicius3_1998@hotmail.com>
2024-07-10 13:57:18 -0300

    primeira parte do mergeSort

--- ordenacaoDeDados/ordenação/mergeSort.py
+++ ordenacaoDeDados/ordenação/mergeSort.py
@@ -1,33 +1,15 @@
-def mergeSort(arr):
-    if len(arr) <= 1:
-        return arr
+def mergeSort(vetor):
+    if len(vetor) <= 1:
+        print(vetor)
+        return vetor
+    
+    meio = len(vetor)//2
 
-    mid = len(arr) // 2
-    leftHalf = arr[:mid]
-    rightHalf = arr[mid:]
+    esquerda = vetor[:meio]
+    direita = vetor[meio:]
 
-    sortedLeft = mergeSort(leftHalf)
-    sortedRight = mergeSort(rightHalf)
+    ordemEsquerda = mergeSort(esquerda)
+    ordemDireita = mergeSort(direita)   
 
-    return merge(sortedLeft, sortedRight)
-
-def merge(left, right):
-    result = []
-    i = j = 0
-
-    while i < len(left) and j < len(right):
-        if left[i] < right[j]:
-            result.append(left[i])
-            i += 1
-        else:
-            result.append(right[j])
-            j += 1
-
-    result.extend(left[i:])
-    result.extend(right[j:])
-
-    return result
-
-unsortedArr = [3, 7, 6, -10, 15, 23.5, 55, -13]
-sortedArr = mergeSort(unsortedArr)
-print("Sorted array:", sortedArr)
+lista = [5, 4, 7, 9, 6, 3, 1, 2, 8]
+print(mergeSort(lista))
