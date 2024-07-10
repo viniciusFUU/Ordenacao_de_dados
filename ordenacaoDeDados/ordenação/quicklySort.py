af5cb91ad695e48af083277445ed83d4ab8df503
Vinícius Monteiro <vinicius3_1998@hotmail.com>
2024-07-10 11:20:48 -0300

    quicklySort concluído

--- ordenacaoDeDados/ordenação/quicklySort.py
+++ ordenacaoDeDados/ordenação/quicklySort.py
@@ -1,45 +1,22 @@
 def quicklySort(vetor):
+    if len(vetor) <= 1:
+        return vetor
+    
     pivo = vetor[0]
 
-    for i in range(len(vetor)):
-        valorAtual = vetor[i]
-        
-        if valorAtual < pivo:
-            vetor.remove(valorAtual)
-            vetor.insert(0, valorAtual)
+    menor = []
+    maior = []
 
-    valorPivo = 0
+    for i in range(1, len(vetor)):
+        if vetor[i] > pivo:
+            maior.append(vetor[i])
+        else:
+            menor.append(vetor[i])
 
-    for n in range(0, pivo):
-        valorPivo+=1
+    menores = quicklySort(menor)
+    maiores = quicklySort(maior)
 
-    valorComparado=valorPivo-2
-
-    if valorPivo >= 0:
-        for i in range(valorPivo-1, -1, -1):
-            pivo=vetor[i-1]
-
-            valorAtual = valorComparado
-            
-            if valorAtual < pivo:
-                vetor.remove(valorAtual)
-                vetor.insert(0, valorAtual)
-                valorComparado-=1
-
-    for i in range(len(vetor) - 1, valorPivo - 1, -1):
-        pivo = vetor[i]
-
-        for j in range(valorPivo, len(vetor)):
-            valorAtual=vetor[j]
-            if pivo > valorAtual:
-                vetor.remove(valorAtual)
-                vetor.insert(valorPivo, valorAtual)
-
-            # if valorAtual < pivo:
-
-
-
-    print(vetor)
+    return menores + [pivo] + maiores
 
 lista = [5, 4, 7, 9, 6, 3, 1, 2, 8]
 print(quicklySort(lista))
