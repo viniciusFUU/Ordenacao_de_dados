288ee26ff9b8e4e71f76b5e743291751443e461f
Vinícius Monteiro <vinicius3_1998@hotmail.com>
2024-09-25 11:08:59 -0300

    Agora o shell sort tá padrão

--- ordenacaoDeDados/ordenação/shellSort.py
+++ ordenacaoDeDados/ordenação/shellSort.py
@@ -1,17 +1,19 @@
 def shellSort(vetor):
     tamanho_vetor = len(vetor)-1
     lista_gap = []
+    gap = tamanho_vetor//2
+
+    while gap > 0:
+        lista_gap.append(gap)
+        gap //=2
+
 
     for x in range(tamanho_vetor):
-        for i in range(tamanho_vetor):
-            gap = tamanho_vetor // 2
-            
-            while(gap > 0):
-                if i+gap <= tamanho_vetor and vetor[i] > vetor[i+gap]:
-                    vetor[i], vetor[i+gap] = vetor[i+gap], vetor[i]
+        for i in range(tamanho_vetor):                
+            for g in lista_gap:        
+                if i+g <= tamanho_vetor and vetor[i] > vetor[i+g]:            
+                    vetor[i], vetor[i+g] = vetor[i+g], vetor[i]
 
-                gap = gap // 2
-        
     return vetor
 
 lista = [51, 46, 73, 90, 66, 38, 13, 22, 87]
