6279c1a05781c11c2306c5eba0f7e7f508ceb402
Vinícius Monteiro <vinicius3_1998@hotmail.com>
2024-09-25 11:25:29 -0300

    insertsort melhorado

--- ordenacaoDeDados/ordenação/insertionSort.py
+++ ordenacaoDeDados/ordenação/insertionSort.py
@@ -1,12 +1,13 @@
 lista = [3,1,5,9,8,2,4,7,6]
 
 def insertSort(vetor):
-    for i in range(1, len(vetor)):
+    tamanho_vetor = len(vetor)
+    for i in range(1, tamanho_vetor):
         posicao_inserir = i
         valor_atual = vetor[i]
         vetor.remove(valor_atual)
 
-        for j in range(len(vetor) - 1, -1, -1):
+        for j in range(i - 1, -1, -1):
             if valor_atual < vetor[j]:
                 posicao_inserir = j
 
