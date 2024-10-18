17ae5f2709b14f2380c1fa9281c69ad7f7ed4a4e
Vinícius Monteiro <vinicius3_1998@hotmail.com>
2024-10-18 15:04:47 -0300

    fazendo um quick no freestyle

--- ordenacaoDeDados/ordenação/quicklySort.py
+++ ordenacaoDeDados/ordenação/quicklySort.py
@@ -1,25 +1,37 @@
 def quickly_sort(vetor):
-    tamanho_vetor = len(vetor)
-
-    if tamanho_vetor < 1:
+    tamanho_lista = len(vetor)-1
+    if tamanho_lista < 1:
         return vetor
     
-    pivo = vetor[0]
+    pivo = vetor[-1]
+    vetor_param = 0
+    vetor_param_novo = 0
 
-    menor = []
-    maior = []
+    for i in range(tamanho_lista):
 
-    for i in range(1, tamanho_vetor):
-        if vetor[i] > pivo:
-            maior.append(vetor[i])
-        else:
-            menor.append(vetor[i])
+        if pivo >= vetor[i]:
+            vetor_param_novo = i
 
-    esquerda = quickly_sort(menor)
-    direita = quickly_sort(maior)
+        if vetor[vetor_param] > vetor[vetor_param_novo]:
+            vetor[vetor_param_novo], vetor[vetor_param] = vetor[vetor_param], vetor[vetor_param_novo]
+            vetor_param+=1
+
+    if vetor[-1] < vetor[vetor_param-1]:
+        vetor[-1], vetor[vetor_param-1] = vetor[vetor_param-1], vetor[-1]
+    
+    print("fim")
+    esquerda = separando_listas(vetor, 0, vetor_param-1)
+    direita = separando_listas(vetor, vetor_param, -1)
+
+    quickly_sort(esquerda)
+    quickly_sort(direita)
 
     return esquerda + [pivo] + direita
 
-lista = [10, 9, 5, 1, 4, 3, 28, 7, 6]
+def separando_listas(vetor, inicio, fim):
+    vetor_adaptado = vetor[inicio:fim]
 
+    return vetor_adaptado
+
+lista = [10, 9, 5, 1, 4, 3, 28, 7, 6]
 print(quickly_sort(lista))
